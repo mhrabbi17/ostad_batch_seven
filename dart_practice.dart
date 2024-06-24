8e891017ccda2aa996164ecfe329636184385cd4
mhrabbi17 <119030264+mhrabbi17@users.noreply.github.com>
2024-06-24 23:12:56 +0600

    Add files via upload
    initial

---
+++ dart_practice.dart
@@ -0,0 +1,26 @@
+class Media{
+  //Media({String s=""});
+  void play(){
+    print("Playing media...");
+  }
+}
+class Song extends Media{
+  String artist = "";
+  Song(String artist){
+    this.artist = artist;
+  }
+
+  @override
+  void play(){
+    print("Playing song by $artist...");
+  }
+}
+void main(){
+  Media m1 = Media();
+  m1.play();
+  Media m2 = Song("Tanzir Tuhin");
+  m2.play();
+
+
+}
+
